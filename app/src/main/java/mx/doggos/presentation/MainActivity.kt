55400f93f0592b3ce9af8ba4084ea4eb6f9b183f
Miguel Gutiérrez <miguenew01@gmail.com>
2022-09-21 18:21:43 -0500

    Move MainActivity.kt to the presentation package

--- app/src/main/java/mx/doggos/presentation/MainActivity.kt
+++ app/src/main/java/mx/doggos/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package mx.doggos
+package mx.doggos.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.doggos.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
